8288e4ba9fc6d558cabd1f024e1265ca086f26d0
daxinte <daxinte@gmail.com>
2018-04-19 20:09:16 +0300

    Detail page

--- pyblog/blog/views.py
+++ pyblog/blog/views.py
@@ -2,13 +2,17 @@
 from __future__ import unicode_literals
 from django.views.generic.list import ListView
 from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404
 from .models import Article
 
 # Create your views here.
 
 def detail(request, article_id):
-    pass
-    
+    details = "blog/detail.html"
+    context = {
+        "article": get_object_or_404(Article, id=article_id)
+    }
+    return render(request, details, context)
 
 class IndexView(ListView):
     model = Article
