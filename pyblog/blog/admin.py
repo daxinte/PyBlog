b837066f37da76106554c0b9bd67c9b30405f12f
daxinte <daxinte@gmail.com>
2018-04-19 17:43:17 +0300

    index article work

--- pyblog/blog/admin.py
+++ pyblog/blog/admin.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import Article, Comment
+
+admin.site.register(Article)
+admin.site.register(Comment)
--- pyblog/blog/views.py
+++ pyblog/blog/views.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.views.generic.list import ListView
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+
+def detail(request, article_id):
+    pass
+    
+
+class IndexView(ListView):
+    model = Article
+
+index = IndexView.as_view()
